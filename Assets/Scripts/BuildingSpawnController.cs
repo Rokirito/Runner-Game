60627fb7e445c5396007c9202a761133b5f23715
Ryudon <antonyurieta@hotmail.com>
2018-12-18 00:39:16 -0500

    Borrando cosas.

--- Assets/Scripts/BuildingSpawnController.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BuildingSpawnController : MonoBehaviour
-{
-    // Buildings and stuff movement speed.
-    public static float speed = 20f;
-}
