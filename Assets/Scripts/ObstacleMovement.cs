40bf19761e8fecffc92b2b5a86fb1b54bc79b037
Renato <renma2497@gmail.com>
2018-12-18 01:03:38 -0500

    Update ObstacleMovement.cs

--- Assets/Scripts/ObstacleMovement.cs
+++ Assets/Scripts/ObstacleMovement.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class ObstacleMovement : MonoBehaviour
 {
-
+    private const int worldSpeed = 20;
 
     void Update()
     {
-        transform.position -= new Vector3(0, 0, (BuildingSpawnController.speed*2) * Time.deltaTime);
+        transform.position -= new Vector3(0, 0, (worldSpeed * 2) * Time.deltaTime);
         if(transform.position.y <= -10)
             Destroy(this.gameObject);
     }
